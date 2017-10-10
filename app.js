ce0779178a120ab516010e88f476002e37afd9f4
Diego Eduardo Rosas González <diego@MacBook-Pro-de-Diego.local>
2017-10-10 18:25:56 -0500

    Quitar ;

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-});
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-});
+})
